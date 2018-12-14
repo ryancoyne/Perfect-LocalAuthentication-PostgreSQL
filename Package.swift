fea3ac92a995560eb106eccdc42af1b25fb1286a
Ryan <ryan@buckettechnologies.com>
2018-12-13 23:22:38 -0500

    Updating to the correct package..

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
 		.Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 3),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 3),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", majorVersion: 3),
-		.Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", majorVersion: 3),
+		.Package(url: "https://github.com/ryancoyne/Postgres-StORM-ryan.git", majorVersion: 3),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session-PostgreSQL.git", majorVersion: 3),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
